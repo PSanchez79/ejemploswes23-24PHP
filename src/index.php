a8f4f44c4ee32e09807a5db526c588929355c7ea
PSanchez79 <psflores79@gmail.com>
2023-09-25 16:51:23 +0200

    Commit ejercicios

--- src/index.php
+++ src/index.php
@@ -1,3 +1,14 @@
 <?php
     echo "Tengo ganas de irme... ";
     ECHO"SI ME QUEREIS ¡¡¡IRSE!!!!";
+
+
+$nombre = "Aitor";
+$nombreCompleto = "Aitor Medrano";
+$numero = 123;
+$numero2 = 456;
+$pi = 3.14;
+$suerte = true;
+$sinValor;
+
+echo $sinValor;
